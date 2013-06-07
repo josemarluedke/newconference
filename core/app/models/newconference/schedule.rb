b3d1497787bf3fd1feebc271714a3ed8acacd47d
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-06-07 17:00:49 -0300

    Create conditional validation for schedule title

--- core/app/models/newconference/schedule.rb
+++ core/app/models/newconference/schedule.rb
@@ -3,5 +3,6 @@ module Newconference
     belongs_to :room
     belongs_to :keynote
     validates :room, :starts_at, presence: true
+    validates :title, presence: true, unless: 'keynote_id.present?'
   end
 end
--- core/spec/models/newconference/schedule_spec.rb
+++ core/spec/models/newconference/schedule_spec.rb
@@ -5,6 +5,16 @@ module Newconference
     describe 'validations' do
       it { should validate_presence_of :room }
       it { should validate_presence_of :starts_at }
+
+      context 'when has a keynote' do
+        subject { Schedule.make keynote: Keynote.make! }
+        it { should_not validate_presence_of :title }
+      end
+
+      context 'when does not have a keynote' do
+        subject { Schedule.make keynote: nil }
+        it { should validate_presence_of :title }
+      end
     end
 
     describe 'associations' do
