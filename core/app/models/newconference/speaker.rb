54d74df6bc73bd04d519be9540ef957e9ad5a3c2
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-06-19 10:13:57 -0300

    Create scope for featured speakers

--- core/app/models/newconference/speaker.rb
+++ core/app/models/newconference/speaker.rb
@@ -3,5 +3,7 @@ module Newconference
     has_many :keynotes, dependent: :destroy
     validates :name, presence: true
     mount_uploader :avatar, Newconference::AvatarUploader
+
+    scope :featured, -> { where(featured: true) }
   end
 end
--- core/spec/models/newconference/speaker_spec.rb
+++ core/spec/models/newconference/speaker_spec.rb
@@ -9,5 +9,16 @@ module Newconference
     describe 'associations' do
       it { should have_many :keynotes }
     end
+
+    describe 'scopes' do
+      describe '.featured' do
+        before do
+          2.times { Speaker.make! featured: true }
+          Speaker.make! featured: false
+        end
+
+        it { expect(Speaker.featured).to have(2).speakers }
+      end
+    end
   end
 end
