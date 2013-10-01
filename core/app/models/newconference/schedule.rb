10a3148cf20da5ee51a4269803a9947241888f5b
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-09-30 21:43:56 -0500

    Add scope by_day

--- core/app/models/newconference/schedule.rb
+++ core/app/models/newconference/schedule.rb
@@ -4,6 +4,7 @@ module Newconference
     belongs_to :keynote
     validates :room, :starts_at, presence: true
     validates :title, presence: true, unless: 'keynote_id.present?'
+    scope :by_day, -> (day) { where('starts_at::date = ?', day) }
 
     def self.days
       select("DISTINCT starts_at::date").map(&:starts_at)
